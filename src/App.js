596a379d54d408bdd574800a91a5b05ec7d59cce
GMorse19 <malorse@comcast.net>
2020-01-21 14:51:05 -0500

    Lessons on 1/20/20

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component'
 import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -18,9 +20,17 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
   render() {
-    return (
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    return ( 
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={e => this.setState({ searchField: e.target.value })}
+      />  
+      <CardList monsters={filteredMonsters} />
       </div>
     );
   }
