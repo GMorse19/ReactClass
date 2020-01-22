acc060e432b815272683c47f219982e8b291a135
GMorse19 <malorse@comcast.net>
2020-01-22 15:05:46 -0500

    Updates

---
+++ precache-manifest.3be580ec36b7696e25781d8301fe4070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0de805b679395647fe665145cdb6ef",
+    "url": "/ReactClass/index.html"
+  },
+  {
+    "revision": "1a764378d43f209d7dce",
+    "url": "/ReactClass/static/css/main.d180572e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/ReactClass/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactClass/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a764378d43f209d7dce",
+    "url": "/ReactClass/static/js/main.582fbff5.chunk.js"
+  },
+  {
+    "revision": "7d88eeb68d99e8039a90",
+    "url": "/ReactClass/static/js/runtime-main.9805217d.js"
+  }
+]);
